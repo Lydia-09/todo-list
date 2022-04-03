8acb545f17b6c642e77713e21b3c026e5cac4e73
Lydia-09 <pasky0901@gmail.com>
2022-04-04 07:04:07 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error')
+})
+
+db.on('open', () => {
+    console.log('mongodb connected!')
+})
+
 const port = 3000
 
 app.get('/', (req, res) => {
