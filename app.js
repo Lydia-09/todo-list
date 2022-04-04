a9a47a7ebbc004b1a19316563fea1cf3e2172804
Lydia-09 <pasky0901@gmail.com>
2022-04-04 17:03:05 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override')
 
 const Todo = require('./models/todo')
 
+const routes = require('./routes')
+
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -27,63 +29,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+app.use(routes)
+
 const port = 3000
-
-app.get('/', (req, res) => {
-    Todo.find()
-        .lean()
-        .sort({ _id: 'asc' })
-        .then(todos => res.render('index', { todos }))
-        .catch(error => console.error(error))
-})
-
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-    const name = req.body.name
-    return Todo.create({ name })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('detail', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('edit', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-    const id = req.params.id
-    const { name, isDone } = req.body
-    return Todo.findById(id)
-        .then(todo => {
-            todo.name = name
-            todo.isDone = isDone === 'on'
-            return todo.save()
-        })
-        .then(() => res.redirect(`/todos/${id}`))
-        .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .then(todo => todo.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
 
 app.listen(port, () => {
     console.log(`App is running on http:/localhost:${port}`)
